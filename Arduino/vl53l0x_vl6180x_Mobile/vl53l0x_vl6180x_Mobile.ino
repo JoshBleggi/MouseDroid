a66365ca53b15e67d58105dd7bf20126c9564b3f
Josh Bleggi <joshbleggi@yahoo.com>
2019-10-03 22:35:53 -0400

    Update vl53l0x_vl6180x_Mobile.ino
    Transition to distance based immobilization detection

--- Arduino/vl53l0x_vl6180x_Mobile/vl53l0x_vl6180x_Mobile.ino
+++ Arduino/vl53l0x_vl6180x_Mobile/vl53l0x_vl6180x_Mobile.ino
@@ -5,7 +5,8 @@ enum SensorState {
   LEFT,
   RIGHT,
   BOTH,
-  NONE
+  NONE,
+  STUCK
 };
 
 // address we will assign if dual sensor is present
@@ -18,10 +19,11 @@ enum SensorState {
 // set the pins to shutdown
 #define SHT_VL 13
 
-const long MILLIS_BEFORE_CHANGE = 2500;
-
-long stateChangeTime = 0;
-SensorState LastState = SensorState::NONE;
+const short MILLIS_BEFORE_CHANGE = 2500;
+short lastMeasurementLeft = 0;
+long lastMeasurementRight = 0;
+long leftStateChangeTime = 0;
+long rightStateChangeTime = 0;
 
 // objects for the vl53l0x
 Adafruit_VL6180X vl = Adafruit_VL6180X();
@@ -32,58 +34,94 @@ VL53L0X_RangingMeasurementData_t measure1;
 
 void loop() {
   SensorState triggeredState = read_dual_sensors();
+  
   delay(100);
   selectDrive(triggeredState);
 }
 
 //Sensor Section
 SensorState read_dual_sensors() {
-  const int VL_MAX = 255, LOX_MAX = 650 ;//8190;
-  int leftSensor, rightSensor;
+  int leftSensorReading, rightSensorReading;
   lox1.getSingleRangingMeasurement(&measure1); // pass in 'true' to get debug data printout!
 
-  leftSensor = measure1.RangeMilliMeter;
-  rightSensor = vl.readRange();
-  bool leftTrigger = leftSensor > 0 && leftSensor < LOX_MAX, rightTrigger = rightSensor > 0 && rightSensor < VL_MAX; 
+  leftSensorReading = measure1.RangeMilliMeter;
+  rightSensorReading = vl.readRange();
   // print sensor one reading
-  Serial.print("Left Sensor: " + String(leftSensor) + " Triggered - " + leftTrigger);
+  Serial.print("Left Sensor: " + String(leftSensorReading));
   
   Serial.print(" ");
 
   // print sensor two reading
-  Serial.print("Right Sensor: " + String(rightSensor) + " Triggered - " + rightTrigger);  
+  Serial.print("Right Sensor: " + String(rightSensorReading));  
   Serial.println();
 
-  return getState(leftTrigger, rightTrigger);
+  return getState(leftSensorReading, rightSensorReading);
 }
 
-void updateSensorState(SensorState newState) {
-  if (newState != LastState) {
-    LastState = newState;
-    stateChangeTime = millis();
+SensorState getState(short leftSensorReading, short rightSensorReading) {
+  const short LEFT_TRIGGER_MAX = 650, RIGHT_TRIGGER_MAX = 255;
+  const short LEFT_MAX_READING = 8190, RIGHT_MAX_READING = 255;
+
+  if (isRobotStuck(leftSensorReading, LEFT_MAX_READING, rightSensorReading, RIGHT_MAX_READING)) {
+    return SensorState::STUCK;
   }
+  
+  bool leftTrigger = leftSensorReading > 0 && leftSensorReading < LEFT_TRIGGER_MAX, rightTrigger = rightSensorReading > 0 && rightSensorReading < RIGHT_TRIGGER_MAX;
+  if (leftTrigger && rightTrigger) {
+    return SensorState::BOTH;
+  }
+  else if (leftTrigger) {
+    return SensorState::LEFT;
+  }
+  else if (rightTrigger) {
+    return SensorState::RIGHT;
+  }
+  else {
+    return SensorState::NONE;
+  }
+}
+
+bool isRobotStuck(short &leftSensorReading, short leftMaxReading, short &rightSensorReading, short rightMaxReading) {
+  if (isDifInThreshold(leftSensorReading, lastMeasurementLeft, leftStateChangeTime, leftMaxReading) || 
+      isDifInThreshold(leftSensorReading, lastMeasurementLeft, leftStateChangeTime, rightMaxReading)) {
+    return true;
+  }
+  return false;
+}
+
+bool isDifInThreshold(short &reading, short &lastMeasurement, long &lastChangeTime, short maxReading) {
+  const byte threshold = 30;
+  short dif = reading - lastMeasurement;
+  if (dif < 0) {
+    dif = dif * -1;
+  }
+  Serial.println("Dif: " + String(dif) + " Last Change Time: " + String(lastChangeTime));
+  if (dif < threshold && reading < maxReading && ((millis() - lastChangeTime) > MILLIS_BEFORE_CHANGE)) {
+    Serial.println("STUCK STUCK STUCK STUCK STUCK");
+    return true;
+  }
+  if (dif > threshold || reading >= maxReading){
+    lastMeasurement = reading;
+    lastChangeTime = millis();
+  }
+  Serial.println("Not stuck");
+  return false;
 }
 
 //Drive Section
 void selectDrive(SensorState triggeredState) {
-  Serial.println("Triggered State: " + String(triggeredState) + " Last State: " + String(LastState));
-  if (triggeredState != LastState) {
-    updateSensorState(triggeredState);
-    
-    if (triggeredState != SensorState::NONE) {
-      Rotate45clockwise();
-    }
-    else {
-      ForwardFullPower();
-    }
+  Serial.println("Triggered State: " + String(triggeredState));
+  if (triggeredState == SensorState::STUCK) {
+    ReverseFullPower();
+    delay(500);
+    Rotate45clockwise();
+    delay(500);
   }
-  else if ((millis() - stateChangeTime) > MILLIS_BEFORE_CHANGE) {
-    if (triggeredState != SensorState::NONE) {
-      ReverseFullPower();
-      delay(500);
-      Rotate45clockwise();
-      delay(500);
-    }
+  if (triggeredState != SensorState::NONE) {
+    Rotate45clockwise();
+  }
+  else {
+    ForwardFullPower();
   }
 }
 
@@ -174,20 +212,4 @@ void initializeMotorPins() {
   pinMode(MOTOR_LEFT_PWM, OUTPUT);
   
   pinMode(12, OUTPUT);
-}
-
-//Helper Section
-SensorState getState(bool leftTrigger, bool rightTrigger) {
-  if (leftTrigger && rightTrigger) {
-    return SensorState::BOTH;
-  }
-  else if (leftTrigger) {
-    return SensorState::LEFT;
-  }
-  else if (rightTrigger) {
-    return SensorState::RIGHT;
-  }
-  else {
-    return SensorState::NONE;
-  }
 }
